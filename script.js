2f018fd829c68a5a2ace34d5b954f41a827e8746
MiguelNPB <miguelnp@ucm.es>
2025-03-22 21:20:26 +0100

    arreglo menu movil a navlinks

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const toggleButton = document.getElementById('toggleButton');
+const hamburguerButton = document.getElementById('hamburguerButton');
 const contentDiv = document.getElementById('contentDiv');
 
-toggleButton.addEventListener('click', () => {
+const containerNav = document.getElementById('navLinksContainer')
+const navLinks = containerNav.getElementsByTagName('a')
+
+hamburguerButton.addEventListener('click', () => {
     contentDiv.classList.toggle('hidden');
+});
+
+Array.from(navLinks).forEach((link) =>{
+    link.addEventListener('click', (event) => {
+    if (window.innerWidth < 575) {
+        contentDiv.classList.toggle('hidden');
+    }
+    })
 });
