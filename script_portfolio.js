df43083a00994aa55438655f3d0ddf65a025c9f1
MiguelNPB <miguelnp@ucm.es>
2025-07-07 00:08:58 +0200

    Subido selector y arreglado scroll portfolio

--- script_portfolio.js
+++ script_portfolio.js
@@ -32,3 +32,70 @@ Array.from(proyectosItems).forEach((div) => {
         linkSrcModal.setAttribute('href', link);
     })
 });
+
+const buttonPortfolioTodos = document.getElementById("portfolio-toggle-todos");
+const buttonPortfolioProgramacion = document.getElementById("portfolio-toggle-programacion");
+const buttonPortfolioArtistatecnico = document.getElementById("portfolio-toggle-artistatecnico");
+const buttonPortfolioOtros = document.getElementById("portfolio-toggle-otros");
+
+function togglePortfolio(type)
+{
+    buttonPortfolioTodos.classList.remove('portfolio-button-selected');
+    buttonPortfolioProgramacion.classList.remove('portfolio-button-selected');
+    buttonPortfolioArtistatecnico.classList.remove('portfolio-button-selected');
+    buttonPortfolioOtros.classList.remove('portfolio-button-selected');
+
+    switch (type)
+    {
+        case "Todos":
+            buttonPortfolioTodos.classList.add('portfolio-button-selected');
+            Array.from(proyectosItems).forEach((div) => {
+                div.style.display = 'flex';
+            });
+            break;
+        case "Programacion":
+            buttonPortfolioProgramacion.classList.add('portfolio-button-selected');
+            Array.from(proyectosItems).forEach((div) => {
+                const img = div.querySelector('img')
+                let type = img.getAttribute("proyect-type");
+
+                if (!type.includes("programacion")){
+                    div.style.display = 'none';
+                } else{
+                    div.style.display = 'flex';
+                }
+            });
+            break;
+        case "ArtistaTecnico":
+            buttonPortfolioArtistatecnico.classList.add('portfolio-button-selected');
+            Array.from(proyectosItems).forEach((div) => {
+                const img = div.querySelector('img')
+                let type = img.getAttribute("proyect-type");
+
+                if (!type.includes("artistatecnico")){
+                    div.style.display = 'none';
+                } else{
+                    div.style.display = 'flex';
+                }
+            });
+            break;
+        case "Otros":
+            buttonPortfolioOtros.classList.add('portfolio-button-selected');
+            Array.from(proyectosItems).forEach((div) => {
+                const img = div.querySelector('img')
+                let type = img.getAttribute("proyect-type");
+
+                if (!type.includes("otros")){
+                    div.style.display = 'none';
+                } else{
+                    div.style.display = 'flex';
+                }
+            });
+            break;
+    }
+}
+
+buttonPortfolioTodos.addEventListener('click', () => togglePortfolio("Todos"));
+buttonPortfolioProgramacion.addEventListener('click', () => togglePortfolio("Programacion"));
+buttonPortfolioArtistatecnico.addEventListener('click', () => togglePortfolio("ArtistaTecnico"));
+buttonPortfolioOtros.addEventListener('click', () => togglePortfolio("Otros"));
